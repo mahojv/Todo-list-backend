330f151225beb24e994aba6ee14f4848f667c63b
mahojv <moisesmjavier@outlook.com>
2025-07-07 20:12:38 -0600

    Se añade funcion GET para solicitar informacion de una Task individual

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-import express from 'express';
 
 const express = require('express');
 const app = express();
@@ -25,11 +24,21 @@ app.get('/tasks', (req, res) => {
     res.json(tasks);
 });
 
+app.get('/tasks/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const task = tasks.find((t) => t.id === id);
+    if (task) {
+        res.json(task);
+    } else {
+        res.status(404).json({ error: 'Task not found' });
+    }
+});
+
 app.post('/tasks', (req, res) => {
     const newTask = {
         id: tasks.length + 1,
-        titulo: req.body.title,
-        descripcion: req.body.description,
+        titulo: req.body.titulo,
+        descripcion: req.body.descripcion,
         estado: false
     };
     tasks.push(newTask);
